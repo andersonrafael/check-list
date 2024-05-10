98e2f09512230322d195804042c7acca998b57bd
anderson rafael <franciscoandersonrafael@gmail.com>
2024-05-10 00:18:41 -0300

    js inputs

---
+++ main.js
@@ -0,0 +1,8 @@
+let tarefasAseFazer = []
+
+const form = document.getElementById("form-itens")
+const itensInput = document.getElementById("receber-item")
+
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault()
+})
